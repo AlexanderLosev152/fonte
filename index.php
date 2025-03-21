cb4c5a3760b62c537b370e41e9cf1377402a4f17
Alexander L <losev.pavlovo152@gmail.com>
2025-03-22 00:14:01 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="header">
+    <div class="container">
+      <div class="header__wrapper">
+        <div class="header__logo">
+          <a href="#">
+            <img src="./assets/images/logo.svg" alt="logo">
+          </a>
+        </div>
+
+        <nav class="nav">
+          <ul>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">Команда</a></li>
+            <li><a href="#">Блог</a></li>
+            <li><a href="#">Продукты</a></li>
+            <li><a href="#">Контакты</a></li>
+          </ul>
+        </nav>
+
+        <button class="mobile-btn">
+          <span>-</span>
+          <span>-</span>
+          <span>-</span>
+        </button>
+      </div>
+    </div> 
+  </header>
+</body>
+</html>
